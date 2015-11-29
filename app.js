d038bdd34b9301b17d085a581593686c93ffbfca
guillaume-boutin <guillaumeboutin2101@gmail.com>
2015-11-29 01:40:01 -0500

    Completed error 400 cases

--- app.js
+++ app.js
@@ -8,30 +8,108 @@ var search = require('./functions/search.js');
 
 app.set('port', port);
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World!');
-});
-
 app.get('/suggestions', function(req, res) {
 
-  // console.log(search(req.query));
+  var passed = false;
+  var message = "";
 
-  var results = search( req.query );
+  var queries = [];
+  var distances = [];
+  var sortings = [];
+  var coordinates = [];
+  var unknowns = [];
 
-  if (results.length === 0) {
-    res.status(404).json({"suggestions": []});
+  for (key in req.query) {
+    if (key === "q" || key === "begins" || key === "ends") {
+      queries.push(req.query.key);
+    }
+    else if (key === "farther" || key === "closer") {
+      distances.push(req.query.key);
+    }
+    else if (key === "distance" || key === "score") {
+      sortings.push(req.query.key);
+    }
+    else if (key === "latitude" || key === "longitude") {
+      coordinates.push(req.query.key);
+    }
+    else {
+      unknowns.push(req.query.key);
+    }
+  }
+
+  if (queries.length === 0) {
+    message = "You must enter either q, begins or end as a name query.";
+  }
+  else if (queries.length > 1) {
+    message = "You can only have one query for name."
+  }
+  else if (distances.length > 0) {
+    if (distances.length > 1) {
+      message = "Query cannot have closer and farther at the same time. Pick only one of them.";
+    }
+    else if (isNaN(req.query[distances[0]])) {
+      message = "closer or farther parameter must be a number";
+    }
+    else {
+      passed = true;
+    }
+  }
+
+  else if (sortings.length > 0) {
+    if (sortings.length > 1) {
+      message = "Cannot sort both distance and score at the same time.";
+    }
+    else if (req.query[sortings[0]] !== "asc" && req.query[sortings[0]] !== "desc") {
+      message = "distance or score can only have values asc or desc.";
+    }
+    else {
+      passed = true;
+    }
+  }
+
+  else if (coordinates.length > 0) {
+    if ( coordinates.length !== 2) {
+      message = "Both latitude and longitude are requires, or none of them."
+    }
+    else if (Number(req.query.latitude) > 90 || Number(req.query.latitude) < -90 || isNaN(req.query.latitude)) {
+      message = "Latitude must be a number in between -90 and 90 inclusively";
+    }
+    else if (Number(req.query.longitude) > 180 || Number(req.query.longitude) < -180 || isNaN(req.query.longitude)) {
+      message = "Longitude must be a number in between -180 and 180 inclusively";
+    }
+    else {
+      passed = true;
+    }
+  }
+
+  else {
+    passed = true;
+  }
+
+  console.log(passed);
+
+  if (!passed) {
+    res.status(400).send("ERROR 400: "+message);
   }
   else {
-    res.status(200).json({"suggestions": results});
-  }
 
+    var results = search( req.query );
+
+    if (results.length === 0) {
+      res.status(404).json({"suggestions": []});
+    }
+    else {
+      res.status(200).json({"suggestions": results});
+    }
+
+  }
   res.end();
 
 })
 
 var server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, '127.0.0.1');
 
 module.exports = server;
 
